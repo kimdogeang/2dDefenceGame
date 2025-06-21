60656705f76e1de2a97070eb2d5c7f25793bf8c1
kimdogeang <peolaeng@gmail.com>
2025-06-21 17:58:54 +0900

    #수업6/21

---
+++ Assets/02_script/CreateEnemy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateEnemy : MonoBehaviour
+{
+    public GameObject prefabEnemy;
+    public Vector2 limitMun;
+    public Vector2 limitMax;
+    private float delay;
+    private int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
