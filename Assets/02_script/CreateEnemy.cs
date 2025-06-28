49f845e12171ecc32e947f9f70a1f3f2ff06bcf2
kimdogeang <peolaeng@gmail.com>
2025-06-28 17:51:30 +0900

    #6/28수ㅗ업

--- Assets/02_script/CreateEnemy.cs
+++ Assets/02_script/CreateEnemy.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CreateEnemy : MonoBehaviour
 {
     public GameObject prefabEnemy;
-    public Vector2 limitMun;
+    public Vector2 limitMin;
     public Vector2 limitMax;
     private float delay;
     private int count;
@@ -13,9 +13,29 @@ public class CreateEnemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine(Create());
+    }
+    IEnumerator Create()
+    {
+        while (true)
+        {
+            count++;
+
+            float r = Random.Range(limitMin.y, limitMax.y);
+            Vector2 creatingPoint = new Vector2(limitMin.x, r);
+
+            Instantiate(prefabEnemy, creatingPoint, Quaternion.identity);
+
+            delay = 10.0f / (count + 4);
+            yield return new WaitForSeconds(delay);
+        }
 
     }
-
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawLine(limitMin, limitMax);
+    }
     // Update is called once per frame
     void Update()
     {
---
+++ Assets/02_script/Zombie_.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie_ : MonoBehaviour
+{
+    Transform tr;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.tr.Translate(Vector2.left * speed * Time.deltaTime);
+            //tr.position = new Vector3(x, y, z);
+            /////////int a = 1
+    }
+}
